ff37462ef1ea4671f10f293d41538a62ec128b49
Dev 012 <dev012@example.invalid>
2025-11-29 04:34:09 +0300

    fix: app startup

--- dev_main.py
+++ dev_main.py
@@ -5,12 +5,14 @@ import uvicorn
 from app.di import tg_webhooks, pgmq
 
 
-async def main() -> None:
+async def prepare_app() -> None:
     set_wh_task = asyncio.create_task(tg_webhooks.set_wh())
     ensure_db_task = asyncio.create_task(pgmq.ensure_database_ready())
-
     await asyncio.gather(set_wh_task, ensure_db_task)
 
+
+if __name__ == "__main__":
+    asyncio.run(prepare_app())
     uvicorn.run(
         "app.app:app",
         host="0.0.0.0",
@@ -19,7 +21,3 @@ async def main() -> None:
         loop="asyncio",
         reload=True,
     )
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import asyncio
 
 import uvicorn
 
-from app.di import tg_webhooks
+from app.di import tg_webhooks, pgmq
 
-WORKERS = 3
+WORKERS = 1
+
+
+async def prepare_app() -> None:
+    set_wh_task = asyncio.create_task(tg_webhooks.set_wh())
+    ensure_db_task = asyncio.create_task(pgmq.ensure_database_ready())
+    await asyncio.gather(set_wh_task, ensure_db_task)
 
 
 if __name__ == "__main__":
-    asyncio.run(tg_webhooks.set_wh())
+    asyncio.run(prepare_app())
     uvicorn.run(
         "app.app:app",
         host="0.0.0.0",
